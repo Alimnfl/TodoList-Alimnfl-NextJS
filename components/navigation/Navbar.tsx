8cecd3d6d52dc814e895731010466e1d6f75b217
Alimnfl <mnaufallim234@gmail.com>
2025-04-17 05:06:57 +0700

    feat: make good condition on that.

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -12,6 +12,7 @@ export default function Navbar() {
   const { tasks } = useTodo();
   const [isOpen, setIsOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const [hydrated, setHydrated] = useState(false);
 
   const [loading, setLoading] = useState(true);
 
@@ -41,12 +42,16 @@ export default function Navbar() {
         controls.set({ x: "0%" });
         setIsOpen(true);
       } else {
-        setIsOpen(true);
+        controls.set({ x: "-100%" });
+        setIsOpen(false);
       }
+
+      setHydrated(true);
     };
 
     checkMobile();
     window.addEventListener("resize", checkMobile);
+
     return () => window.removeEventListener("resize", checkMobile);
   }, [controls]);
 
@@ -62,7 +67,7 @@ export default function Navbar() {
       <div
         {...handlers}
         className={`fixed top-0 left-0 h-full ${
-          isOpen ? "w-full" : "w-[10%]"
+          isOpen ? "w-full" : "w-[4%]"
         } z-20 md:hidden`}
       />
 
@@ -72,43 +77,44 @@ export default function Navbar() {
           onClick={() => toggleSidebar(false)}
         />
       )}
-
-      <motion.div
-        animate={isMobile ? controls : { x: "0%" }}
-        initial={{ x: !isOpen ? "-100%" : "0%" }}
-        className={`z-20 fixed md:static top-0 left-0 h-full w-64 bg-white shadow-md p-6 flex flex-col gap-3 min-h-screen md:translate-x-0 md:shadow-none min-w-[350px]`}
-      >
-        <div className="py-10 flex flex-col gap-2 w-full">
-          <div className="flex text-2xl font-semibold flex-row gap-3 bg-green-500 w-fit rounded-xl px-2 py-1 text-white">
-            <span>@</span>
-            <h2 className="">Todo Alimnfl</h2>
+      {hydrated && (
+        <motion.div
+          animate={isMobile ? controls : { x: "0%" }}
+          initial={{ x: "-100%" }}
+          className={`z-20 fixed md:static top-0 left-0 h-full w-64 bg-white shadow-md p-6 flex flex-col gap-3 min-h-screen md:translate-x-0 md:shadow-none min-w-[350px]`}
+        >
+          <div className="py-10 flex flex-col gap-2 w-full">
+            <div className="flex text-2xl font-semibold flex-row gap-3 bg-green-500 w-fit rounded-xl px-2 py-1 text-white">
+              <span>@</span>
+              <h2 className="">Todo Alimnfl</h2>
+            </div>
+            <span className="text-gray-600 sm:text-base text-sm">
+              Your companion for a more organized journey
+            </span>
           </div>
-          <span className="text-gray-600 sm:text-base text-sm">
-            Your companion for a more organized journey
-          </span>
-        </div>
 
-        <div className="flex flex-col w-full py-4 gap-2 min-h-[300px]">
-          {loading
-            ? Array?.from({ length: 2 })?.map((_, index) => (
-                <CardCategorySkeleton create={false} key={index} />
-              ))
-            : tasks?.map((data, index) => (
-                <CardCategory
-                  toggleSidebar={toggleSidebar}
-                  create={false}
-                  data={data}
-                  key={index}
-                />
-              ))}
-        </div>
+          <div className="flex flex-col w-full py-4 gap-2 min-h-[300px]">
+            {loading
+              ? Array?.from({ length: 2 })?.map((_, index) => (
+                  <CardCategorySkeleton create={false} key={index} />
+                ))
+              : tasks?.map((data, index) => (
+                  <CardCategory
+                    toggleSidebar={toggleSidebar}
+                    create={false}
+                    data={data}
+                    key={index}
+                  />
+                ))}
+          </div>
 
-        {loading ? (
-          <CardCategorySkeleton create={true} />
-        ) : (
-          <ModalAddCategory />
-        )}
-      </motion.div>
+          {loading ? (
+            <CardCategorySkeleton create={true} />
+          ) : (
+            <ModalAddCategory />
+          )}
+        </motion.div>
+      )}
 
       {isMobile && !isOpen && (
         <button
